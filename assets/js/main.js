4d9a8f3dee67487abb74a53deb0d386f8cf8dc1f
Alek90 <4lek.90@gmail.com>
2021-10-12 16:54:58 +0200

    Tentativo inefficace.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,11 @@
 // Analizzando il file style.css appare la necessità di applicare una classe .active al div.hamburger-menu del file html per rendere funzionante la proprietà applicata in fase responsive.
 
 // Strumenti
+
+const element = document.getElementsByClassName(".hamburger-menu");
+
+const button = document.getElementsByClassName(".close");
+
+element.addEventListener("click", function() {
+        element.className = `${element.classList} active`;
+    })
