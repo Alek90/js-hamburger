036b99601c5718e674c8baf5321b263e578155a7
Alek90 <4lek.90@gmail.com>
2021-10-12 17:32:38 +0200

    Consegna esercizio.
    Ripulito il codice, sistemati i commenti, inseriti i ;.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,24 +2,24 @@
 
 // Analizzando il file style.css appare la necessità di applicare una classe .active al div.hamburger-menu del file html per rendere funzionante la proprietà applicata in fase responsive.
 
-// Strumenti
+// Variabili
 
 const element = document.querySelector(".hamburger-menu");
 
-const icon = document.querySelector(".fa-bars")
+const icon = document.querySelector(".fa-bars");
 
 const button = document.querySelector(".close");
 
 
+// Eventi
 
 icon.addEventListener("click", function() {
-       /*  element.className = `${element.classList} active`; */
         
     element.classList.add("active")
-})
+});
 
 
 button.addEventListener("click", function(){
 
     element.classList.remove("active")
-})
+});
