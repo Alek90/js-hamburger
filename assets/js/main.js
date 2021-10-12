66545ce0127bc5dc2faccb89d2b3558325e9e7d0
Alek90 <4lek.90@gmail.com>
2021-10-12 16:12:40 +0200

    Analisi dei codici.
    Analizzati i codici prescritti, ed identificato il punto di intervento.

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+// OBBIETTIVO: mostrare - nascondere menù 
+
+// Analizzando il file style.css appare la necessità di applicare una classe .active al div.hamburger-menu del file html per rendere funzionante la proprietà applicata in fase responsive.
+
+// Strumenti
